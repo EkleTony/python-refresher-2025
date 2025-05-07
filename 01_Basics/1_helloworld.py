4910b7b90bd4462c72a912b1e055308aba5866c8
ANTHONY EKLE <ekleanthony5@gmail.com>
2025-05-07 03:05:07 -0500

    updated

---
+++ 01_Basics/1_helloworld.py
@@ -0,0 +1,9 @@
+# Here is Linkedln Learning Pythong course by Joe Marini
+# I'm reveiwing the course to refresh my memory haha
+# simple print, variable and input keyword
+
+print("Hello world")
+
+# user input
+name = input("What is your name? ")
+print("Hello ", name,
