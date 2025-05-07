b8b9a54a6a7791ebdcf13e1769aa52dbb2755cf8
ANTHONY EKLE <ekleanthony5@gmail.com>
2025-05-07 03:43:08 -0500

    updated note

--- 01_Basics/2_variables_data.py
+++ 01_Basics/2_variables_data.py
@@ -8,29 +8,4 @@ my_str = "this is a String bro"
 my_bool = False
 
 
-# Operators are used to perform operations on variables
-print("=============== Operators ================")
-print(my_int + my_float)
-print(my_int * my_float)
-print(my_int / my_float)
-print("modulo", my_int % 2)
-
-print("============= String operations=========")
-another_str = "This is another string"
-print(my_str + another_str)
-print("nom" * 3)
-
-# Logical and Comparison Operators
-print("=============== Logical and Comparison operator========")
-print(my_int == 10)
-print(my_float != 20)
-print(my_int > 20)
-print(my_float < 20)
-
-print(not (my_int > 5 and my_float < 30.0))
-
-
-# Re-declaring variables
-print("==== Variable re-declaring=======")
-my_int = "abc"
-print(my_int)
+#
---
+++ 01_Basics/3_sequence_start.py
@@ -0,0 +1,5 @@
+# Sequences: LIst and tuples
+
+mylist = [0, 1, 2, "one", False]
+print("my list is : ", mylist)
+print(
