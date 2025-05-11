1789de8d70b262c98e675d30e6912a7d7e6a1968
ANTHONY EKLE <ekleanthony5@gmail.com>
2025-05-11 02:44:28 -0500

    updated

--- 02_Beyond_Basics/5_code_challenge.py
+++ 02_Beyond_Basics/5_code_challenge.py
@@ -20,13 +20,24 @@ def count_numbers(which, numbers):
             return -1
 
 
+# optimal solution
+
+def count_number2(which, numbers):
+    if which == "even":
+        return sum(1 for i in numbers if i % 2 == 0)
+    elif which == "odd":
+        return sum(1 for i in numbers if i % 2 == 1)
+    else:
+        return -1
+
+
 numbers = [7, 17, 2, 13, 19, 20, 0, 5, 11, 1280, 105]
 # print(f"even count: {count_numbers("even", numbers)}")
-ans = count_numbers("even", numbers)
+ans = count_number2("even", numbers)
 print(f"even count: ", ans)
 
-ans2 = count_numbers("odd", numbers)
+ans2 = count_number2("odd", numbers)
 print(f"odd counts: ", ans2)
 
-ans3 = count_numbers("which", numbers)
+ans3 = count_number2("which", numbers)
 print("none: ", ans3)
