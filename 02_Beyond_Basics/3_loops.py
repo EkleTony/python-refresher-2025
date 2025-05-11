5baaadb4e2a09138fa7ae5250519e6fc34230aad
ANTHONY EKLE <ekleanthony5@gmail.com>
2025-05-11 00:06:55 -0500

    upated file

---
+++ 02_Beyond_Basics/3_loops.py
@@ -0,0 +1,35 @@
+# ============ Loops ====================
+
+# 1. Whhile loop: executive if a certain condtion i true
+x = 0
+while x < 5:
+    print(x, "Welcome to school! ")
+    x += 1
+
+print()
+print("========== Stop conditions =============")
+
+# ans = input("Should I stop? ").lower()
+# while ans != "yes":
+#     print(ans)
+#     ans = input("should I stop? ").lower()
+
+
+print()
+print("====Multiplication Table ====")
+# Nested While loop
+i = 1
+while i <= 12:
+    # print(f"{i} times Table")
+    j = 1
+    while j <= 15:
+        print(f"{i*j} ", end='\t')
+        j += 1
+    print()
+    i += 1
+
+print("========== Multiplication TAble with for loop=======")
+for i in range(1, 13):
+    for j in range(1, 13):
+        print(f"{i*j}", end=" \t")
+    print()
