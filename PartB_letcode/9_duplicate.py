c0cdfc8fb4f46aa6ef58580462e263394443435c
ANTHONY EKLE <ekleanthony5@gmail.com>
2025-05-17 17:56:11 -0500

    updated files

--- PartB_letcode/9_duplicate.py
+++ PartB_letcode/9_duplicate.py
@@ -1,10 +1,29 @@
-#  --- ============= Given an Array nums, return true if any value appears at least twice in the array, 
+#  --- ============= Given an Array nums, return true if any value appears at least twice in the array,
 # and return false if every element is distinct
 
 
 #
 #
-input = nums = [4, 3, 2, 7, 8, 2, 3, 1]
+from collections import Counter
+input = nums = [4, 3, 2, 7, 8, 3, 1]
+
+count = Counter(nums)
+# duplicates = [strTrue for k, v in count.items() if v > 1] # O(n)
+
+
+def findDuplicate(nums):
+    count = Counter(nums)
+    print(count)
+    for k, v in count.items():
+
+        if v >= 1:
+            return True
+        else:
+            return False
+
+
+x = findDuplicate(nums)
+print(x)
 
 
 # output [2, 3
