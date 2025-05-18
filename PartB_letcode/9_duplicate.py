73c3d5f5e0169fbea1b5de844eb74dd1dd053a51
ANTHONY EKLE <ekleanthony5@gmail.com>
2025-05-18 01:13:47 -0500

    updated

--- PartB_letcode/9_duplicate.py
+++ PartB_letcode/9_duplicate.py
@@ -4,6 +4,7 @@
 
 #
 #
+from collections import defaultdict
 from collections import Counter
 input = nums = [4, 3, 2, 7, 8, 3, 1]
 
@@ -25,5 +26,25 @@ def findDuplicate(nums):
 x = findDuplicate(nums)
 print(x)
 
+# ========== DefualtDict
 
+d = defaultdict(list)
+d["names"].append("Anthony")
+d['age'].append(29)
+d['school'].append("TTU")
+print(f"defualt dict: {d.keys()}")
+
+
+# ========= default_factory arg of int
+
+dic = defaultdict(int)
+nums = [1, 2, 1, 3, 4, 5, 2, 1, 2, 3, 6, 7, 8, 2, 3, 2, 1, 2]
+
+for i in nums:
+    dic[i] += 1
+frq = [k for k, v in dic.items() if v == max(dic.values())]
+print(f"Num Freq: {frq[0]}")
+
+# O(n) i.e counting and list comprehension O(k)
+# Space is O(k)
 # output [2, 3
--- PartC_Arrays/4_counters.py
+++ PartC_Arrays/4_counters.py
@@ -18,3 +18,31 @@ for _ in range(100):
 
 print(dct)
 
+
+# ======================
+# find the item wiht highest frequency
+nums = [2, 3, 4, 5, 6, 2, 3, 5, 6, 5, 2, 2]
+count = Counter(nums)
+x = max(count.values())
+
+for key in count:
+    print(f"Item {key} has the frequency {count[key]}")
+print(f"maximum freq item is: {[k for k, v in count.items() if v == x]}")
+print(f"max freq: {count.most_common(3)}")
+
+
+# ========== Strings =========
+
+string = "Hi there I am sample text. I am just sample repeat what i want to say there hi you are gine text right, just asking, thanks"
+#print(string)
+# spliting and lower case
+list_string = string.lower().split(" ")
+#print(list_string)
+
+str_count = Counter(list_string)
+print(str_count)
+
+print(f"lenght is {len(str_count)}")
+
+# most common 
+print(str_count.most_common(4))
