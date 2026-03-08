bdfcc1cb37fb0f133590a9cdb9c8e9a8c360551a
ANTHONY EKLE <ekleanthony5@gmail.com>
2026-03-07 18:51:40 -0600

    Reviewing TwoSum Leetcode Problem

--- PartB_Neetcode150/31_TwoSumReview.py
+++ PartB_Neetcode150/31_TwoSumReview.py
@@ -5,7 +5,7 @@
 ---such that they add up to `target`
 
 """
-#---Attermpt 
+# ---Attermpt
 
 
 def twoSum1(arr1, target):
@@ -16,19 +16,21 @@ def twoSum1(arr1, target):
 
 # method 2 using pointers
 
+
 def twoSum2(arr1, target):
     l = 0
-    r = len(arr1) -1
-    while l<r:
-        sum1 = arr1[l] + arr1[r]
-        if sum1 == target:
+    r = len(arr1) - 1
+    while l < r:
+        summ = arr1[l] + arr1[l]
+        if summ == target:
             return [l, r]
         else:
-            if sum1 < target:
+            if summ < target:
                 l += 1
             else:
                 r -= 1
 
-arr1 = [2, 3, 4, 5, 7, 5,6,8,9]
-target = 15
+
+arr1 = [2, 3, 4, 5, 7, 5, 6, 8, 9]
+target = 5
 print("Two Sum and Target==", twoSum2(arr1, target))
