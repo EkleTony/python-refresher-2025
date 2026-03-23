1c5e37d2c0644761a188a436faccc3701852e6c0
ANTHONY EKLE <ekleanthony5@gmail.com>
2026-03-23 14:41:31 -0500

    Euclidean Distance - Practical Machine Learning Tutorial

---
+++ PartE_Linkedln2026/11_ML_euclideanDist.py
@@ -0,0 +1,12 @@
+""" Euclidean Distance - Practical Machine Learning Tutorial with Python p.15
+--- Used mostly for KNN-algorithmss...!
+ """
+
+
+def euclidean_distance(p1,p2):
+    # Sum of n(i-to-n)--, 
+    # euc_dist = np.sqrt( (p1-p2)**2)
+    pass
+
+
+    
