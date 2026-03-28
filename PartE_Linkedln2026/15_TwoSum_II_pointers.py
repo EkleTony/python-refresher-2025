de53c0d3b886ecbaf5f08d34faac1a5b89b757fa
ANTHONY EKLE <ekleanthony5@gmail.com>
2026-03-28 14:06:38 -0500

    Two Sum with Sorted Array and using two pointers--updated

--- PartE_Linkedln2026/15_TwoSum_II_pointers.py
+++ PartE_Linkedln2026/15_TwoSum_II_pointers.py
@@ -17,8 +17,10 @@ def twoSum_sorted(nums, target):
     #             return [i, j]
 
     # ========= Using two pointers ==========
+    # [1,2,3,4] t =4
+    #  l     r -- if s= num[l] + num[r] == t, return else if s<t, increase l else reduce r
     left = 0
-    right = len(nums) - 1
+    right = len(nums)-1
 
     while left < right:
         sum1 = nums[left] + nums[right]
@@ -39,5 +41,57 @@ print(
 
 
 """ =====wwhile unSorted Array uses Hashmap and enumerate, complement way """
-arr2 = [3, 2, 4, 1]
-target2 = 5
+
+arr2 = [1, 3, 2, 4, 7, 5, 1, 6]
+#       0  1  2  3  4  5  6  7
+target2 = 6
+print(f'Unsorted array: {twoSum_sorted(arr2, target2)}')
+print("======= Building Method for unsorted array using hash map.. ")
+
+
+def twoSum_unsorted(nums, target):
+    # build a hashmap or dict
+    # enumerate and find complement
+    # check if the compllement + any eleemnt == target
+    # return that value whose complent and the num (incie)
+    seen = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+
+        # else update
+        seen[num] = i
+
+
+print(f'Unsorted array--Failed Solution: {twoSum_sorted(arr2, target2)}')
+
+print(f'Unsorted array--Better Solution: {twoSum_unsorted(arr2, target2)}')
+
+
+
+
+""" ===== Next on two pointers
+
+2. 3Sum (VERY important) (sort + fix one element
+two pointers inside)
+
+3. 3. Container With Most WaterProblem (Max area between two vertical lines
+Pattern
+start wide (edges)
+move the smaller height inward
+
+ 4. Remove Duplicates from Sorted Array
+ 
+ 
+ 5. 5. Valid Palindrome
+Problem
+
+Check if string is palindrome (ignore non-alphanumeric)
+
+Pattern
+left/right pointers
+skip invalid chars
+
+
+"""
