0f154bd9712668dd2bb01c9f886fd725f18b2696
ANTHONY EKLE <ekleanthony5@gmail.com>
2026-03-28 04:36:54 -0500

    Updated code for Find K Closest Elements - Leetcode 658

--- PartE_Linkedln2026/13_FindKClosestElement.py
+++ PartE_Linkedln2026/13_FindKClosestElement.py
@@ -17,11 +17,29 @@ def find_closest_element1(nums, k, x):
     # sort by cloesest to x
     # break ties by smaller values and take first k, then sort
     # result = sorted(nums, key=lambda num: (abs(num - x), num))[:k]
+
     keys = (abs(nums-x), nums)
     return sorted(nums, key=keys)
 
 
+# ======== Sliding window examples====
+def sortedSquare(nums):
+    res = []
+    for x in nums:
+        res.append(x**2)
+    #res.sort()
+    return sorted(res)
+# time complexity === O(nlog n)--- becuase of sorting
+# needs O(n) with pointers ===
+
+
+print("======== Sliding Window================")
+print(f'Squared of sorted array: {sortedSquare([1, -4, -2, 3, 5])}')
+print()
+
 # ========== Method 2: Using binary search
+
+
 def find_closest_element(nums, k, x):
     left = 0
     right = len(nums) - k
