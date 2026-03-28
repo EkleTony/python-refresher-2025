ef713523825eb723930a01c64ccf27ee283e9776
ANTHONY EKLE <ekleanthony5@gmail.com>
2026-03-28 05:36:24 -0500

    Two Sum with Sorted Array and using two pointers

--- PartE_Linkedln2026/15_TwoSum_II_pointers.py
+++ PartE_Linkedln2026/15_TwoSum_II_pointers.py
@@ -7,15 +7,35 @@ Q: Given a sorted array, find two numbers that sum to a target.
 arr1 = [1, 2, 3, 4, 5]
 target1 = 5
 
-# brute force
-
 
 def twoSum_sorted(nums, target):
-    n = len(nums)
-    for i in range(n):
-        for j in range(i, n):
-            if nums[i] + nums[j] == target:
-                return [i, j]
+    # ===========brute-force==========
+    # n = len(nums)
+    # for i in range(n):
+    #     for j in range(i, n):
+    #         if nums[i] + nums[j] == target:
+    #             return [i, j]
+
+    # ========= Using two pointers ==========
+    left = 0
+    right = len(nums) - 1
+
+    while left < right:
+        sum1 = nums[left] + nums[right]
+        if sum1 == target:
+            return [left, right]
+        else:
+            if sum1 < target:
+                left += 1
+            else:
+                right -= 1
+
+
+print(
+    f'Using Sorted Array {arr1} and  target {target1} Brute-Force: {twoSum_sorted(arr1, target1)}')
+
+
+# ========now using Two pointers=========
 
 
 """ =====wwhile unSorted Array uses Hashmap and enumerate, complement way """
