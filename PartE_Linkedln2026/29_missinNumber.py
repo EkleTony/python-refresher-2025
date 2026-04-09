8f8217ebc3f6ccbbda4b9c06c132dc51d17bfb94
ANTHONY EKLE <ekleanthony5@gmail.com>
2026-04-09 16:32:40 -0500

    Missing Number

---
+++ PartE_Linkedln2026/29_missinNumber.py
@@ -0,0 +1,24 @@
+
+
+def missingNum1(num):
+    n = max(num)
+    expected = n*(n+1)//2
+    actual = sum(num)
+    missing = expected - actual
+    return missing
+
+
+print(f"missing num of {1, 2, 4, 5} is {missingNum1([1, 2, 4, 5])}")
+
+
+def missingMultiple(num):
+    seen = set(num)
+    n = 6
+    missing = []
+    for i in range(1, n+1):
+        if i not in seen:
+            missing.append(i)
+    return missing
+
+
+print(f'Missing Numbers are in [1,3,4,6]: {missingMultiple([1, 2, 4, 6])}')
