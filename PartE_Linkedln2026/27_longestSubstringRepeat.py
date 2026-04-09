d410d8abdc1469ee913eee1b648c4c321ca5f482
ANTHONY EKLE <ekleanthony5@gmail.com>
2026-04-09 16:31:52 -0500

    Longest Substring leetcode 3

---
+++ PartE_Linkedln2026/27_longestSubstringRepeat.py
@@ -0,0 +1,24 @@
+""" Longest Substring Without Repeating Characters - Leetcode 3 - Python
+
+
+"""
+st = "abcabcdef"
+# max_len = 3
+
+
+def longestSubstring(st):
+    char_set = set()
+    left = 0
+    max_subLenght = 0
+
+    for right in range(len(st)):
+        while st[left] in char_set:
+            char_set.remove(st[left])
+            left += 1
+
+        char_set.add(st[right])
+        max_subLenght = max(max_subLenght, right-left + 1)
+    return max_subLenght
+
+
+print(f"Longest Substring of {st} is == {longestSubstring(st)}")
